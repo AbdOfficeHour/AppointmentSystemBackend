616061480d328f63c1c009db00c9db9cad7bb3e8
csf123321 <2652212759@qq.com>
2024-10-25 01:21:50 +0800

    本地模拟SSO的状态

--- appointmentSystem/src/main/java/io/github/abdofficehour/appointmentsystem/filter/CorsFilter.java
+++ appointmentSystem/src/main/java/io/github/abdofficehour/appointmentsystem/filter/CorsFilter.java
@@ -24,7 +24,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin","http://officehour.abdn.kirisame.cc");
+        response.setHeader("Access-Control-Allow-Origin","http://localhost:8031");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
